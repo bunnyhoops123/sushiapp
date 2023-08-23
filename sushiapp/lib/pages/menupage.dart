3eba9190e4cb37988670f5f404fdb8ef8ec4ebd8
bunnyhoops123 <sumukh.varmay@gmail.com>
2023-08-23 11:42:14 +0530

    FoodTile, menuPage, Trending

--- sushiapp/lib/pages/menupage.dart
+++ sushiapp/lib/pages/menupage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sushiapp/components/button.dart';
+import 'package:sushiapp/components/foodtile.dart';
 import 'package:sushiapp/models/food.dart';
 import 'package:sushiapp/themes/colors.dart';
 
@@ -38,72 +39,139 @@ class _MenuPageState extends State<MenuPage> {
           style: TextStyle(color: Colors.grey[900]),
         ),
       ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Container(
-          decoration: BoxDecoration(
-              color: primaryColor, borderRadius: BorderRadius.circular(20)),
-          margin: EdgeInsets.symmetric(horizontal: 10),
-          padding: EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Get 32% Promo',
-                    style: GoogleFonts.dmSerifDisplay(
-                        fontSize: 20, color: Colors.white),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.circular(20)),
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  padding: EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Get 32% Promo',
+                            style: GoogleFonts.dmSerifDisplay(
+                                fontSize: 20, color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          MyButton(text: "REDEEM", onTap: () {})
+                        ],
+                      ),
+                      Image.asset(
+                        'assets/many-sushi.png',
+                        height: 100,
+                      )
+                    ],
                   ),
-                  SizedBox(
-                    height: 20,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search here ..",
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(20)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(20)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
                   ),
-                  MyButton(text: "REDEEM", onTap: () {})
-                ],
-              ),
-              Image.asset(
-                'assets/many-sushi.png',
-                height: 100,
-              )
-            ],
-          ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Text(
+                    'Food Menu',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey[800],
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 25.0, right: 25, bottom: 20),
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: foods.length,
+                        itemBuilder: (context, index) => FoodTile(
+                              food: foods[index],
+                            )),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 25, left: 25, right: 25),
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/salmon-eggs.png',
+                            height: 60,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Salmon Eggs',
+                                style: GoogleFonts.dmSerifDisplay(fontSize: 19),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '\$21.00',
+                                style: TextStyle(color: Colors.grey[700]),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(
+                        Icons.favorite_border_outlined,
+                        color: Colors.grey[700],
+                      )
+                    ],
+                  ),
+                )
+              ]),
         ),
-        SizedBox(
-          height: 25,
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(20)),
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(20)),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(20)),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 25,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Text(
-            'Food Menu',
-            style: TextStyle(
-                fontSize: 20,
-                color: Colors.grey[800],
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-      ]),
+      ),
     );
   }
 }
